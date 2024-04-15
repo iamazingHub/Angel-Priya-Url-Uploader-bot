3e99de30cf42aa966105bb618cbbea517e1a07ad
iamazingHub <85914808+iamazingHub@users.noreply.github.com>
2024-04-16 04:27:27 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -34,6 +34,12 @@ async def echo(bot, update):
     if update.from_user.id in Config.AUTH_USERS:
         logger.info(update.from_user)
         url = update.text
+        # Check if the update object has the 'message_id' attribute
+        if hasattr(update, 'message_id'):
+            reply_to_message_id = update.message_id
+        else:
+            reply_to_message_id = None
+
         youtube_dl_username = None
         youtube_dl_password = None
         file_name = None
@@ -82,7 +88,7 @@ async def echo(bot, update):
                     video=dldir,
                     caption=file_name,
                     duration=duration,
-                    reply_to_message_id=update.message_id,
+                    reply_to_message_id=reply_to_message_id,
                     progress=progress_for_pyrogram,
                     progress_args=(
                         Translation.UPLOAD_START,
@@ -96,7 +102,7 @@ async def echo(bot, update):
                     audio=dldir,
                     caption=file_name,
                     duration=duration,
-                    reply_to_message_id=update.message_id,
+                    reply_to_message_id=reply_to_message_id,
                     progress=progress_for_pyrogram,
                     progress_args=(
                         Translation.UPLOAD_START,
@@ -109,7 +115,7 @@ async def echo(bot, update):
                     chat_id=update.chat.id,
                     document=dldir,
                     caption=file_name,
-                    reply_to_message_id=update.message_id,
+                    reply_to_message_id=reply_to_message_id,
                     progress=progress_for_pyrogram,
                     progress_args=(
                         Translation.UPLOAD_START,
@@ -202,7 +208,7 @@ async def echo(bot, update):
             await bot.send_message(
                 chat_id=update.chat.id,
                 text=Translation.NO_VOID_FORMAT_FOUND.format(str(error_message)),
-                reply_to_message_id=update.message_id,
+                reply_to_message_id=reply_to_message_id,
                 parse_mode="html",
                 disable_web_page_preview=True
             )
@@ -334,7 +340,7 @@ async def echo(bot, update):
                 Config.CHUNK_SIZE,
                 None,  # bot,
                 Translation.DOWNLOAD_START,
-                update.message_id,
+                reply_to_message_id,
                 update.chat.id
             )
             if os.path.exists(thumb_image_path):
@@ -347,7 +353,7 @@ async def echo(bot, update):
                 text=Translation.FORMAT_SELECTION.format(thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
                 reply_markup=reply_markup,
                 parse_mode="html",
-                reply_to_message_id=update.message_id
+                reply_to_message_id=reply_to_message_id
             )
         else:
             # fallback for nonnumeric port a.k.a seedbox.io
@@ -372,5 +378,5 @@ async def echo(bot, update):
                 text=Translation.FORMAT_SELECTION.format(""),
                 reply_markup=reply_markup,
                 parse_mode="html",
-                reply_to_message_id=update.message_id
+                reply_to_message_id=reply_to_message_id
             )

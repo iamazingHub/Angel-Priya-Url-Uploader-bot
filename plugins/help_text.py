ea82a7971c819d58831979b95691d2fc7bb8f20b
iamazingHub <85914808+iamazingHub@users.noreply.github.com>
2024-04-16 04:36:51 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -78,7 +78,7 @@ async def start(bot, update):
             reply_to_message_id=update.message_id
         )
     else:
-        # logger.info(update) ==         
+        # logger.info(update)
         await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.LAZY_START_TEXT.format(update.from_user.first_name),
